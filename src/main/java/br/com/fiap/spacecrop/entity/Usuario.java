a3cf5ff474fb2555527e40754c7b791f790780a6
lgaxd <lucasgrilloxd@gmail.com>
2026-05-27 23:53:24 -0300

    Adicionando arquivos entity

--- src/main/java/br/com/fiap/spacecrop/entity/Usuario.java
+++ src/main/java/br/com/fiap/spacecrop/entity/Usuario.java
@@ -1,35 +1,56 @@
 package br.com.fiap.spacecrop.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "TB_USUARIO")
 @Data
-@Table(name = "tb_usuario")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @EqualsAndHashCode(of = "id")
+@ToString(exclude = {"fazendas", "alertas", "acoes"})
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_USUARIO")
+    @SequenceGenerator(name = "SEQ_USUARIO", sequenceName = "SEQ_USUARIO", allocationSize = 1)
+    @Column(name = "id_usuario")
     private Long id;
 
-    @Column(nullable = false, length = 100)
-    private String name;
+    @Column(name = "nm_usuario", nullable = false, length = 100)
+    private String nome;
 
-    @Column(unique = true, nullable = false, length = 100)
+    @Column(name = "ds_email", nullable = false, unique = true, length = 150)
     private String email;
 
-    @Column(name = "ds_senha_hash", nullable = false)
-    private String passwordHash;
-    
+    @Column(name = "ds_senha_hash", nullable = false, length = 255)
+    private String senhaHash;
+
+    @JsonIgnore
+    @Builder.Default
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Fazenda> fazendas = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Alerta> alertas = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<AcaoAlerta> acoes = new ArrayList<>();
+
 }
